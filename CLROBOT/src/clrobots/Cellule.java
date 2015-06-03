c0f9d38da1ee0a25edc42735c76f000d550fc56e
ctalon <claude.talon@gmail.com>
2015-06-03 22:50:10 +0200

    [CT] Random box generation & Cellule

---
+++ CLROBOT/src/clrobots/Cellule.java
@@ -0,0 +1,22 @@
+package clrobots;
+
+public class Cellule {
+	private int positionX;
+	private int positionY;
+	
+	
+	
+	public Cellule(int x, int y){
+		positionX = x;
+		positionY = y;
+	}
+	
+	public int getPositionX(){
+		return positionX;
+	}
+	
+	public int getPositionY(){
+		return positionY;
+	}
+
+}
--- CLROBOT/src/clrobots/impl/EnvironnementImpl.java
+++ CLROBOT/src/clrobots/impl/EnvironnementImpl.java
@@ -1,11 +1,19 @@
 package clrobots.impl;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import clrobots.Boite;
 import clrobots.Environnement;
 import clrobots.interfaces.IEnvInfos;
 import clrobots.interfaces.Igui;
 import clrobots.interfaces.Iinteragir;
 
 public class EnvironnementImpl extends Environnement{
+	
+	private List<Boite> boiteList = new ArrayList<Boite>();
 
 	@Override
 	protected Igui make_gui() {
@@ -25,8 +33,22 @@ public class EnvironnementImpl extends Environnement{
 		return null;
 	}
 
-
-	
-
+	public void addBoite(){
+		Color color;
+		int x, y;
+		Random rand = new Random();
+		float randomValue = rand.nextFloat();
+		if (randomValue < 0.33){
+			color = Color.RED;
+		} else if (randomValue >= 0.33 && randomValue < 0.66) {
+			color = Color.GREEN;
+		} else {
+			color = Color.BLUE;
+		}
+		x = rand.nextInt(50) + 1;
+		y = rand.nextInt(50) + 1;
+		Boite boite = new Boite(color,x,y);
+		boiteList.add(boite);
+	}
 
 }
